0e95a5d2e154ec564fa9a9137a1a2fe523eab8b5
Jacob Boyd <jacoboyd611@outlook.com>
2021-02-24 14:40:19 -0500

    yurt

--- LostV2/Form1.Designer.cs
+++ LostV2/Form1.Designer.cs
@@ -30,99 +30,150 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.outputLabel = new System.Windows.Forms.Label();
-            this.redLabel = new System.Windows.Forms.Label();
-            this.blueLabel = new System.Windows.Forms.Label();
-            this.blueImage = new System.Windows.Forms.PictureBox();
-            this.redImage = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.blueImage)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.redImage)).BeginInit();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.option3Label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // outputLabel
             // 
-            this.outputLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputLabel.ForeColor = System.Drawing.Color.White;
-            this.outputLabel.Location = new System.Drawing.Point(24, 28);
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.outputLabel.Font = new System.Drawing.Font("DejaVu Sans Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.Black;
+            this.outputLabel.Location = new System.Drawing.Point(401, 9);
             this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(339, 257);
+            this.outputLabel.Size = new System.Drawing.Size(374, 280);
             this.outputLabel.TabIndex = 2;
-            this.outputLabel.Text = "label1";
+            this.outputLabel.Text = "You are riding the subway by yourself, on your way home from work when a rat jump" +
+    "s up on the seat next to yours. \r\n\r\nDo you move seats or shoo the rat away?";
             // 
-            // redLabel
+            // option1Label
             // 
-            this.redLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.redLabel.ForeColor = System.Drawing.Color.White;
-            this.redLabel.Location = new System.Drawing.Point(67, 313);
-            this.redLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.redLabel.Name = "redLabel";
-            this.redLabel.Size = new System.Drawing.Size(257, 23);
-            this.redLabel.TabIndex = 3;
-            this.redLabel.Text = "label1";
+            this.option1Label.BackColor = System.Drawing.Color.White;
+            this.option1Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.option1Label.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option1Label.Font = new System.Drawing.Font("DejaVu Sans Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.ForeColor = System.Drawing.Color.Black;
+            this.option1Label.Location = new System.Drawing.Point(444, 303);
+            this.option1Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(331, 32);
+            this.option1Label.TabIndex = 3;
+            this.option1Label.Text = "Move seats";
+            this.option1Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // blueLabel
+            // option2Label
             // 
-            this.blueLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.blueLabel.ForeColor = System.Drawing.Color.White;
-            this.blueLabel.Location = new System.Drawing.Point(67, 349);
-            this.blueLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.blueLabel.Name = "blueLabel";
-            this.blueLabel.Size = new System.Drawing.Size(257, 23);
-            this.blueLabel.TabIndex = 4;
-            this.blueLabel.Text = "label1";
+            this.option2Label.BackColor = System.Drawing.Color.White;
+            this.option2Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.option2Label.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option2Label.Font = new System.Drawing.Font("DejaVu Sans Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.ForeColor = System.Drawing.Color.Black;
+            this.option2Label.Location = new System.Drawing.Point(444, 347);
+            this.option2Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(331, 32);
+            this.option2Label.TabIndex = 5;
+            this.option2Label.Text = "Shoo away the rat";
+            this.option2Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // blueImage
+            // option3Label
             // 
-            this.blueImage.Image = global::LostV2.Properties.Resources.blue_50x50;
-            this.blueImage.Location = new System.Drawing.Point(24, 339);
-            this.blueImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.blueImage.Name = "blueImage";
-            this.blueImage.Size = new System.Drawing.Size(39, 32);
-            this.blueImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.blueImage.TabIndex = 1;
-            this.blueImage.TabStop = false;
+            this.option3Label.BackColor = System.Drawing.Color.White;
+            this.option3Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.option3Label.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option3Label.Font = new System.Drawing.Font("DejaVu Sans Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option3Label.ForeColor = System.Drawing.Color.Black;
+            this.option3Label.Location = new System.Drawing.Point(444, 390);
+            this.option3Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.option3Label.Name = "option3Label";
+            this.option3Label.Size = new System.Drawing.Size(331, 32);
+            this.option3Label.TabIndex = 6;
+            this.option3Label.Visible = false;
             // 
-            // redImage
+            // label1
             // 
-            this.redImage.Image = global::LostV2.Properties.Resources.red_50x50;
-            this.redImage.Location = new System.Drawing.Point(24, 303);
-            this.redImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.redImage.Name = "redImage";
-            this.redImage.Size = new System.Drawing.Size(39, 32);
-            this.redImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.redImage.TabIndex = 0;
-            this.redImage.TabStop = false;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(401, 303);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "A";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(401, 347);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "D";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(401, 390);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 32);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "S";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(386, 422);
-            this.Controls.Add(this.blueLabel);
-            this.Controls.Add(this.redLabel);
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(786, 433);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.option3Label);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
             this.Controls.Add(this.outputLabel);
-            this.Controls.Add(this.blueImage);
-            this.Controls.Add(this.redImage);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Lost in a Forest";
+            this.Text = "The Subway";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            ((System.ComponentModel.ISupportInitialize)(this.blueImage)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.redImage)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox redImage;
-        private System.Windows.Forms.PictureBox blueImage;
         private System.Windows.Forms.Label outputLabel;
-        private System.Windows.Forms.Label redLabel;
-        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Label option3Label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- LostV2/Form1.cs
+++ LostV2/Form1.cs
@@ -10,16 +10,19 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;  
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
 
 namespace LostV2
 {
     public partial class Form1 : Form
     {
         // tracks what part of the game the user is at
-        int scene = 0;  
-
+        int scene = 0;
         // random number generator
+        Random randGen = new Random();
 
         public Form1()
         {
@@ -32,20 +35,20 @@ namespace LostV2
         {
             /// check to see what button has been pressed and advance
             /// to the next appropriate scene
-            if (e.KeyCode == Keys.M)       //red button press
+            if (e.KeyCode == Keys.A)       //Option A
             {
-                else if (scene == 0) { }
-                else if (scene == 1) { }
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene = 4; }
                 else if (scene == 2) { }
                 else if (scene == 3) { }
-                else if (scene == 4) { }
+                else if (scene == 4) { scene = 0; }
                 else if (scene == 5) { }
                 else if (scene == 6) { }
             }
-            else if (e.KeyCode == Keys.B)  //blue button press
+            else if (e.KeyCode == Keys.D)  //Option B
             {
-                else if (scene == 0) { }
-                else if (scene == 1) { }
+                if (scene == 0) { scene = 2; }
+                else if (scene == 1) { scene = 2; }
                 else if (scene == 2) { }
                 else if (scene == 3) { }
                 else if (scene == 4) { }
@@ -56,21 +59,153 @@ namespace LostV2
             /// Display text and game options to screen based on the current scene
             switch (scene)
             {
-                case 0:  //start scene  
+                case 0:
+                    outputLabel.Text = $"You are riding the subway by yourself, on your way home from work when a rat jumps up on the seat next to yours.\n\nDo you move seats or shoo the rat away?";
+                    option1Label.Text = "Move seats";
+                    option2Label.Text = "Shoo away the rat";
+                    Refresh();
                     break;
-                case 1:   
+                case 1:
+                    outputLabel.Text = $"The rat scampers around and you eye it carefully from across the aisle. A second joins the first on the seat across from you, but it's eyes seem to be fixated on you.\n\nDo you ignore the rats or shoo them away?";
+                    option1Label.Text = "Ignore the rats";
+                    option2Label.Text = "Shoo them away";
+                    Refresh();
                     break;
                 case 2:
+                    outputLabel.Text = $"The rat runs off down the subway car and disappears. Shortly after you hear a scratching noise coming from behind your head.\n\nDo you inspect the panel behind your seat or continue to do nothing?";
+                    option1Label.Text = "Inspect the pannel";
+                    option2Label.Text = "Do nothing";
+                    Refresh();
                     break;
                 case 3:
+                    outputLabel.Text = $"The noise get so loud that it sounds like the panel is going to burst. Do you inspect the panel behind your seat or continue to do nothing?";
+                    option1Label.Text = "Inspect the pannel";
+                    option2Label.Text = "Do nothing";
+                    Refresh();
                     break;
                 case 4:
+                    outputLabel.Text = $"You and the rats sit in peace for the rest of the subway ride and you arrive at your destination.\n\nGame Over \n\nPlay again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    Refresh();
                     break;
                 case 5:
+                    outputLabel.Text = $"You pull the panel off the side of the subway car. There's nothing but wires and electronics, the car is now dead silent.\n\nDo you put the pannel back on the wall or leave it off and sit back down";
+                    option1Label.Text = "Put the pannel back on the wall";
+                    option2Label.Text = "Leave the pannel off and sit back down";
+                    Refresh();
                     break;
                 case 6:
+                    outputLabel.Text = $"By some unknown force the panel flies off the wall into the back of your head, rendering you unconscious.\n\nGame Over\n\n Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    Refresh();
                     break;
                 case 7:
+                    outputLabel.Text = $"After putting the panel back you turn around and face a horde of rats standing stock still. They cover the entire subway car and when you move to get away the horde pounces. Killing you with no witnesses.\n\nGame Over\n\nPlay Again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    Refresh();
+                    break;
+                case 8:
+                    outputLabel.Text = $"You turn around and face a horde of rats standing stock still. Your instincts tell you to grab hold of the metal panel.\n\n Do you hold up the pannel in defense or throw it at the rats?";
+                    option1Label.Text = "Defend with the pannel";
+                    option2Label.Text = "Throw it at the rats";
+                    Refresh();
+                    break;
+                case 9:
+                    outputLabel.Text = $"You manage to block the first swarm of rats buying you some time but more quickly latch on your ankles.\n\nDo you attempt to kill the remaining rats or look for a way out?";
+                    option1Label.Text = "Try to kill the rats";
+                    option2Label.Text = "Look for a way out";
+                    Refresh();
+                    break;
+                case 10:
+                    outputLabel.Text = $"The horde backs off, almost absorbing your feeble attempt, but it buys you some time.\n\n You scan the cramped subway car for an escape. You have 2 options, break the window and jump out of a moving subway car, attempt to run to the next car in search of other people.\n\nWhich do you pick?";
+                    option1Label.Text = "Break Window";
+                    option2Label.Text = "Run to the next car";
+                    Refresh();
+                    break;
+                case 11:
+                    outputLabel.Text = $"You try to kill the remaining rats but they overpower you, eating at your flesh while you suffocate from their sheer volume.\n\nGame Over\n\n Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "Yes";
+                    Refresh();
+                    break;
+                case 12:
+                    outputLabel.Text = $"You scan the cramped subway car for an escape. You have 2 options, break the window and jump out of a moving subway car, attempt to run to the next car in search of other people.\n\nWhich do you pick?";
+                    option1Label.Text = "Break Window";
+                    option2Label.Text = "Run to the next car";
+                    Refresh();
+                    break;
+                case 13:
+                    outputLabel.Text = $"You jump out the window and crack your skull open on the tracks.\n\n Game Over \n\n Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    //Refresh();
+                    break;
+                case 14:
+                    outputLabel.Text = $"You trip and fall, the rats overpower you eating at your flesh while you suffocate from their sheer volume.\n\nGame Over\n\n Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    Refresh();
+                    break;
+                case 15:
+                    outputLabel.Text = $"You make it to the next car and slam the door behind you. You're safe from the rats for now. You look to the floor and realize that the other passengers have been killed and are covered in bite marks. The rats have almost broke through the door.\n\nDo you hide with the bodies or go to the driver's compartment to stop the train";
+                    option1Label.Text = "Go into the driver's conpartment";
+                    option2Label.Text = "Hide with the bodies";
+                    Refresh();
+                    break;
+                case 16:
+                    outputLabel.Text = $"Miraculously you escape. You run as fast as you can and come to a 3 way crossroad.\n\nWhich direction do you pick?";
+                    option1Label.Text = "Left";
+                    option2Label.Text = "Straight";
+                    option3Label.Visible = true;
+                    label3.Visible = true;
+                    option3Label.Text = "Right";
+                    Refresh();
+                    break;
+                case 17:
+                    outputLabel.Text = $"You enter the drivers compartment and pull the emergency break lever. You leave the train and run as fast as you can. You quickly come across a 3 way crossroad.\n\nWhich direction do you pick?";
+                    option3Label.Visible = true;
+                    label3.Visible = true;
+                    option3Label.Text = "Right";
+                    Refresh();
+                    break;
+                case 18:
+                    outputLabel.Text = $"You close your eyes and lay unmoving, waiting for the swarm to wash over you, but they never do.\n\nDo you continue to lay there or get up?";
+                    option1Label.Text = "Get up";
+                    option2Label.Text = "Lay there";
+                    Refresh();
+                    break;
+                case 19:
+                    outputLabel.Text = $"You hear a ding and the doors open. You've made the next stop. You get out of the train and pass out on the platform.\n\nYou wake up in handcuffs and a man in a uniform tells you you're being tried for the murder of 12 people. The court deems you mentally insane and images of rats traumatize you in your jail cell.\n\nGame Over \n\nPlay again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    Refresh();
+                    break;
+                case 20:
+                    outputLabel.Text = $"You turn left and follow the tunnel for what feels like hours. Eventually light hits your eyes and you come across a subway platform. A passerby helps you off the tracks and calls an ambulance.\n\nAt the hospital no one believes your story but they fix you up.After a few days you're released and back to normal. Though you vow never to take the subway again.\n\nGame Over\n\nPlay again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    option3Label.Visible = false;
+                    label3.Visible = false;
+                    Refresh();
+                    break;
+                case 21:
+                    outputLabel.Text = $"You continue straight. Light begins to creep around the corner of the tunnel, a subway platform? Nope. The cold hard steel of a subway train smashes you into a million pieces.\n\nGame Over\n\nPlay again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    option3Label.Visible = false;
+                    label3.Visible = false;
+                    Refresh();
+                    break;
+                case 22:
+                    outputLabel.Text = $"You turn right and follow the tunnel. After hours of exhaustion you break your ankle on an unknown object shrouded in darkness. Unable to continue you breakdown and wait for the unavoidable death by subway train that comes for you.\n\nGame Over\n\nPlay again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    option3Label.Visible = false;
+                    label3.Visible = false;
+                    Refresh();
                     break;
                 default:
                     break;
